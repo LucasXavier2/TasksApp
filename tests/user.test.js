d8738f73249bcc31e1ab874549504e7766ed870b
Lucas Xavier <lucasxavier2@gmail.com>
2022-04-06 13:45:36 -0300

    test: add test for user login. Success and failure

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,8 +2,15 @@ const request = require('supertest');
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+const userOne = {
+    name: 'John',
+    email: 'john@example.com',
+    password: 'John12345.'
+}
+
 beforeEach(async () => {
     await User.deleteMany()
+    await new User(userOne).save()
 })
 
 test('Should signup a new user', async () => {
@@ -12,4 +19,18 @@ test('Should signup a new user', async () => {
         email: 'lucas@lucas.com',
         password: '1234567'
     }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Should not login nonexisting user', async () => {
+    await request(app).post('/users/login').send({
+        email: 'idontexist@example.com',
+        password: '987654321'
+    }).expect(400)
 })
